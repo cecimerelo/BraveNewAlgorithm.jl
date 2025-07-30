43249ccef0a5e3d1518b09571af85322cb08c065
JJ Merelo <jjmerelo@gmail.com>
2025-07-30 19:51:38 +0200

    #21 experimenting with parallelization by using @spawn
    After making this question (so far unanswered) https://stackoverflow.com/questions/79720321/julia-code-seems-to-take-longer-the-longer-the-number-of-threads-its-been-run I surrounded the code with @time and realized that there was some small time saving, but in fact a lot of time was spent as compilation time. So I tried to dummy-call the function before, so that it did not get compiled in every single call
    
    Long story short, compilation time went really down from 330% compilation time to a more reasonable 30-40% depending on the number of threads
    
    The issue is that, despite what time returns, wallclock time might still be a lot...

---
+++ examples/multi_only_crossover.jl
@@ -0,0 +1,33 @@
+using Pkg
+Pkg.activate(".")
+include("../src/BraveNewAlgorithm.jl")
+using .BraveNewAlgorithm
+using BlackBoxOptimizationBenchmarking
+using Base.Threads
+
+include("../src/utils.jl")
+include("../src/methods/fertilising_room.jl")
+include("./get_offspring.jl")
+
+const POPULATION_SIZE = 1000000
+
+@info "Number of threads -> $(nthreads())"
+config_file_path = "./test/Config Files/config_file_1_test.json"
+config_parameters_entity = read_parameters_file(config_file_path)
+fitness_function = BlackBoxOptimizationBenchmarking.BBOBFunctions[1]
+range = (-5.12, 5.12)
+minimum_comparator = comparator(element, fitness_function) = element >= fitness_function.f_opt
+ff = FitnessFunction(BlackBoxOptimizationBenchmarking.BBOBFunctions[1])
+population_model = PopulationModel(config_parameters_entity, ff, range, minimum_comparator)
+
+embryos = [fertilising_room(population_model) for _ in 1:POPULATION_SIZE]
+dummy_offspring = get_offspring((embryos[1], embryos[2]))
+@time begin
+    chunks = Iterators.partition(embryos, length(embryos) ÷ nthreads())
+    tasks = map(chunks) do chunk
+        @spawn get_offspring(chunk)
+    end
+    all_offspring = vcat([fetch(task) for task in tasks]...)
+end
+
+@info "All offspring -> $(length(all_offspring))"
