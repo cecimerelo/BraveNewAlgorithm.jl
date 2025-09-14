25985df4ec90e61bbdc3a2267e9537b4351b2b0d
JJ Merelo <jjmerelo@gmail.com>
2025-09-14 21:17:27 +0200

    :recycle: to clarify

--- src/operators/mutation.jl
+++ src/operators/mutation.jl
@@ -1,4 +1,9 @@
-function mutation_operator(offspring, mutation_rate)
+"""
+    mutation_operator(offspring, mutation_percentage)
+
+Apply mutation to `offspring` based on `mutation_percentage`.
+"""
+function mutation_operator(offspring, mutation_percentage)
     mutated_offspring = Array{Float64,1}()
     genes_to_mutate = floor(Int, mutation_rate * length(offspring) / 100)
     indexes_to_mutate = rand(1:length(offspring), genes_to_mutate)
