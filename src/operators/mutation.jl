a49e53ad05218f73b6c7f96539e5ac3eaee57bd8
JJ Merelo <jjmerelo@gmail.com>
2025-09-15 13:56:37 +0200

    closes #34 clarifying via name

--- src/operators/mutation.jl
+++ src/operators/mutation.jl
@@ -5,7 +5,7 @@ Apply mutation to `offspring` based on `mutation_percentage`.
 """
 function mutation_operator(offspring, mutation_percentage)
     mutated_offspring = Array{Float64,1}()
-    genes_to_mutate = floor(Int, mutation_rate * length(offspring) / 100)
+    genes_to_mutate = floor(Int, mutation_percentage * length(offspring) / 100)
     indexes_to_mutate = rand(1:length(offspring), genes_to_mutate)
 
     for (index, gene) in enumerate(offspring)
