004167d81cd788660218af05fed2360c96de51f9
JJ Merelo <jjmerelo@gmail.com>
2025-09-19 20:43:24 +0200

    :construction:

--- test/individuals_test.jl
+++ test/individuals_test.jl
@@ -8,9 +8,9 @@ range = (-5.12, 5.12)
 
 @testset "Test that Embryo is created with correct values" begin
     ff_called = 0
-    for i in [2, 3, 5, 10, 20, 40]
+    for size in [2, 3, 5, 10, 20, 40]
         for j in 1:10
-            chromosome = generate_chromosome(range, i)
+            chromosome = generate_chromosome(range, size)
             embryo = Embryo(chromosome, fitness_function)
             @test typeof(embryo) <: Embryo
             @test embryo.f_value > 0
@@ -21,9 +21,9 @@ range = (-5.12, 5.12)
 end
 
 @testset "Test that for this specific function fitness values for any kind of chromosome are always > 0" begin
-    for i in [2, 3, 5, 10, 20, 40]
+    for size in [2, 3, 5, 10, 20, 40]
         for j in 1:10
-            chromosome = generate_chromosome(range, i)
+            chromosome = generate_chromosome(range, size)
             f_value = fitness_function(chromosome)
             @test f_value > 0
         end
