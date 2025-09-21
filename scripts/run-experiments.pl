de21eef4a9da04054847d63376042d036185630b
JJ Merelo <jjmerelo@gmail.com>
2025-09-21 10:29:17 +0200

    #51 :construction:

--- scripts/run-experiments.pl
+++ scripts/run-experiments.pl
@@ -10,40 +10,48 @@ use Utils qw(process_pinpoint_output);
 
 my $preffix = shift || die "I need a prefix for the data files";
 my $data_dir = shift || "data";
-my $function = shift || "generation";
+my $function = shift || "bna";
 
 my $ITERATIONS = 30;
 my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)/;
 my $suffix = "$day-$mon-$hh-$mm-$ss";
 
 open my $fh, ">", "$data_dir/$preffix-$function-$suffix.csv";
-say $fh "work,type,size,PKG,seconds";
+say $fh "work,type,size,max_gens,alpha,PKG,seconds,generations,diff_fitness,evaluations";
 
-my $work = $function ne "generation" ? "-f $function":"";
-
-for my $t ( qw(f d ) ) {
-  for my $l ( qw(128 256 512) ) {
-    my $total_seconds;
-    my $successful = 0;
-    my @results;
-    do {
-      my $command = "./fp -i $l -t $t $work";
-      say $command;
-      my $output = `pinpoint -- $command 2>&1`;
-      say $output;
-      my ( $gpu, $pkg, $seconds ) = process_pinpoint_output $output;
-      if ($gpu != 0 ) {
-        $successful++;
-        $total_seconds += $seconds;
-        say "$preffix, $function, $t,  $l,$pkg, $seconds";
-        push @results, [$pkg,$seconds];
-      }
+for my $t ( qw(3  ) ) {
+  for my $l ( qw(200 ) ) {
+    for my $max_gens ( qw(10 ) ) {
+      for my $alpha ( qw(10 ) ) {
+        my $total_seconds;
+        my $successful = 0;
+        my @results;
+        do {
+          my $command = "/home/jmerelo/.juliaup/bin/julia examples/simple_test.jl $t $l $max_gens $alpha";
+          say $command;
+          my $output = `pinpoint -- $command 2>&1`;
+          say $output;
+          my ( $gpu, $pkg, $seconds ) = process_pinpoint_output $output;
+          if ($gpu != 0 ) {
+            $successful++;
+            $total_seconds += $seconds;
+            say "$preffix, $function, $t,  $l,$pkg, $seconds";
+            my ($generations, $best_fitness, $target_fitness, $evaluations ) = process_bna_output( $output );
+            push @results, [$pkg,$seconds,$generations, $target_fitness-$best_fitness , $evaluations];
+          }
     } while ( $successful < $ITERATIONS );
 
     foreach  my $row (@results) {
       say join(", ", @$row);
-      say $fh "$function, $t, $l, ", join(", ", @$row);
+      say $fh "$function, $t, $l, $max_gens, $alpha, ", join(", ", @$row);
     }
   }
 }
 close $fh;
+
+sub process_bna_output {
+  my $output = shift;
+  my ($generations, $best_fitness, $target_fitness, $evaluations ) =
+    $output =~ /generations:\s+(\d+).+Best fitness: (\S+).+fitness: (\S+).+evaluations: (\d+)/s;
+  return $generations, $best_fitness, $target_fitness, $evaluations
+}
