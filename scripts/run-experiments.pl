f455b30792be7cd2dba5bf401f55037591a6eab5
JJ Merelo <jjmerelo@gmail.com>
2025-09-21 10:39:39 +0200

    #51 ready to run experiments

--- scripts/run-experiments.pl
+++ scripts/run-experiments.pl
@@ -19,10 +19,10 @@ my $suffix = "$day-$mon-$hh-$mm-$ss";
 open my $fh, ">", "$data_dir/$preffix-$function-$suffix.csv";
 say $fh "work,type,size,max_gens,alpha,PKG,seconds,generations,diff_fitness,evaluations";
 
-for my $t ( qw(3  ) ) {
-  for my $l ( qw(200 ) ) {
-    for my $max_gens ( qw(10 ) ) {
-      for my $alpha ( qw(10 ) ) {
+for my $t ( qw(3 5) ) {
+  for my $l ( qw(200 400) ) {
+    for my $max_gens ( qw(10 50) ) {
+      for my $alpha ( qw(10 25) ) {
         my $total_seconds;
         my $successful = 0;
         my @results;
@@ -37,7 +37,7 @@ for my $t ( qw(3  ) ) {
             $total_seconds += $seconds;
             say "$preffix, $function, $t,  $l,$pkg, $seconds";
             my ($generations, $best_fitness, $target_fitness, $evaluations ) = process_bna_output( $output );
-            push @results, [$pkg,$seconds,$generations, $target_fitness-$best_fitness , $evaluations];
+            push @results, [$pkg,$seconds,$generations, $best_fitness-$target_fitness , $evaluations];
           }
         } while ( $successful < $ITERATIONS );
 
