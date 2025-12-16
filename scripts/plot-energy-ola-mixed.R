e80d5599d8f524f9ca401c03e3e23f9ce09750ac
JJ Merelo <jjmerelo@gmail.com>
2025-12-16 10:14:21 +0100

    #65 script to check stuff out

--- scripts/plot-energy-ola-mixed.R
+++ scripts/plot-energy-ola-mixed.R
@@ -1,17 +1,20 @@
-data_3 <- read.csv("data/ola-base-ola-baseline-14-Dec-12-06-42.csv")
-data_4 <- read.csv("data/ola-1.11.7-v2-baseline-v2-14-Dec-20-40-47.csv")
+mixed_data <- read.csv("data/ola-1.11.7-mixed-ola-mixed-15-Dec-19-49-11.csv")
+mixed_data$delta_seconds <- 0
+mixed_data$delta_PKG <- 0
 
-data_3$group <- 2
-data_4$group <- 3
-
-data <- rbind(data_4,data_3)
+for (i in 2:nrow(mixed_data)) {
+  if (mixed_data$work[i] == "ola-mixed") {
+    mixed_data$delta_seconds[i] <- mixed_data$seconds[i] - mixed_data$seconds[i-1]
+    mixed_data$delta_PKG[i] <- mixed_data$PKG[i] - mixed_data$PKG[i-1]
+  }
+}
 
 library(ggplot2)
 
-data$color <- ifelse(data$group == 2, "red", "blue")
-data$shape <- ifelse(data$dimension == 3, 21,23)
-ggplot(data, aes(x = seconds, y = PKG)) +
-  geom_point(color=data$color, shape=data$shape,size=data$population_size/100) +
+mixed_data$color <- ifelse(mixed_data$population_size == 200, "red", "blue")
+mixed_data$shape <- ifelse(mixed_data$dimension == 3, 21,23)
+ggplot(mixed_data, aes(x = delta_seconds, y = delta_PKG)) +
+  geom_point(color=mixed_data$color, shape=mixed_data$shape) +
   labs(
     title = "Energy Consumption Over Time",
     x = "Time",
@@ -31,13 +34,15 @@ ggplot(data, aes(x = factor(population_size), y = PKG, color=factor(dimension)))
   theme_minimal()
 
 library(dplyr)
-summary_data <- data %>%
-  group_by(population_size, dimension, group) %>%
+summary_data <- mixed_data %>%
+  group_by(work,population_size, dimension, max_gens) %>%
   summarise(
     mean_PKG = mean(PKG),
     median_PKG = median(PKG),
     sd_PKG = sd(PKG),
-    trimmed_PKG = mean(PKG, trim = 0.2)
+    trimmed_PKG = mean(PKG, trim = 0.2),
+    mean_delta_PKG = mean(delta_PKG, trim=0.2),
+    sd_delta_PKG = sd(delta_PKG)
   )
 
 for (dim in c(3,5)) {
