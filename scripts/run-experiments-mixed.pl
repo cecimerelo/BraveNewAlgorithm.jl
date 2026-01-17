966f43c27603eb7f2db29ab45d1c5c2b83463b03
JJ Merelo <jjmerelo@gmail.com>
2026-01-17 16:49:12 +0100

    #77 :recycle: to use a sandwich baseline

--- scripts/run-experiments-mixed.pl
+++ scripts/run-experiments-mixed.pl
@@ -26,23 +26,10 @@ for my $t ( qw(10 5 3) ) {
     for my $max_gens ( qw(10 25) ) {
       for ( my $i = 0; $i < $ITERATIONS; $i++ ) {
         for my $baseline ( qw( 1 0 ) ) {
-          my $pre_preffix = ($baseline eq "1")?"base-" : "";
-          say "\nRunning $pre_preffix";
-          my ( $gpu, $pkg, $seconds, $output );
-          my $command = "/home/jmerelo/.juliaup/bin/julia examples/BBOB_sphere_with_baseline.jl $t $l $max_gens $alpha".($baseline ? " 1" : "");
-          say $command;
-          do {
-            $output = `pinpoint -i 100 -- $command 2>&1`;
-            say $output;
-            ( $gpu, $pkg, $seconds ) = process_pinpoint_output $output;
-          } while ( $gpu == 0 );
-          say "$pre_preffix$preffix, $function, $t,  $l,$pkg, $seconds";
-          my ($generations, $best_fitness, $target_fitness, $evaluations ) = process_bna_output( $output );
-          my @results = ($pkg,$seconds,$generations, $best_fitness-$target_fitness, $evaluations);
-          say join(", ", @results);
-          say $fh "$pre_preffix$function, $t, $l, $max_gens, $alpha, ", join(", ", @results);
+          run_command_for_preffix( $fh, $t, $l, $max_gens, $baseline );
         }
       }
+      run_command_for_preffix( $fh, $t, $l, $max_gens, 0 );
     }
   }
 }
@@ -54,4 +41,23 @@ sub process_bna_output {
   my ($generations, $best_fitness, $target_fitness, $evaluations ) =
     $output =~ /generations:\s+(\d+).+Best fitness: (\S+).+fitness: (\S+).+evaluations: (\d+)/s;
   return $generations, $best_fitness, $target_fitness, $evaluations
+}
+
+sub run_command_for_preffix {
+  my ($fh, $t, $l, $max_gens, $baseline) = @_;
+  my $pre_preffix = ($baseline eq "1")?"base-" : "";
+  say "\nRunning $pre_preffix";
+  my ( $gpu, $pkg, $seconds, $output );
+  my $command = "/home/jmerelo/.juliaup/bin/julia examples/BBOB_sphere_with_baseline.jl $t $l $max_gens $alpha".($baseline ? " 1" : "");
+  say $command;
+  do {
+    $output = `pinpoint -i 100 -- $command 2>&1`;
+    say $output;
+    ( $gpu, $pkg, $seconds ) = process_pinpoint_output $output;
+  } while ( $gpu == 0 );
+  say "$pre_preffix$preffix, $function, $t,  $l,$pkg, $seconds";
+  my ($generations, $best_fitness, $target_fitness, $evaluations ) = process_bna_output( $output );
+  my @results = ($pkg,$seconds,$generations, $best_fitness-$target_fitness, $evaluations);
+  say join(", ", @results);
+  say $fh "$pre_preffix$function, $t, $l, $max_gens, $alpha, ", join(", ", @results);
 }
