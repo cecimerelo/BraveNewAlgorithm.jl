abb6477470e60b3ac81386ba4af89ff4cf3ace6f
JJ Merelo <jjmerelo@gmail.com>
2026-02-28 10:16:43 +0100

    #96 starting tests

--- scripts/sandwich-with-sensors-laptop.pl
+++ scripts/sandwich-with-sensors-laptop.pl
@@ -6,7 +6,7 @@ use warnings;
 use v5.14;
 
 use lib qw(lib ../lib ../../lib);
-use Utils qw(process_pinpoint_output process_sensors_output);
+use Utils qw(process_pinpoint_output_intel process_sensors_output_intel);
 
 my $preffix = shift || die "I need a prefix for the data files";
 my $function = shift || "bna";
@@ -18,7 +18,7 @@ my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)
 my $suffix = "$day-$mon-$hh-$mm-$ss";
 
 open my $fh, ">", "$data_dir/$preffix-$function-$suffix.csv";
-say $fh "work,dimension,population_size,max_gens,alpha,PKG,seconds,generations,diff_fitness,evaluations,initial_temp";
+say $fh "work,dimension,population_size,max_gens,alpha,RAM,PKG,seconds,generations,diff_fitness,evaluations,initial_temp";
 
 my $alpha = 10;
 for my $t ( qw(10 5 3) ) {
@@ -54,17 +54,17 @@ sub run_command_for_preffix {
   my ($fh, $t, $l, $max_gens, $baseline, @initial_temperature) = @_;
   my $pre_preffix = ($baseline eq "1")?"base-" : "";
   say "\nRunning $pre_preffix";
-  my ( $gpu, $pkg, $seconds, $output );
+  my ( $foo, $ram, $gpu, $pkg, $seconds, $output );
   my $command = "/home/jmerelo/.juliaup/bin/julia examples/BBOB_sphere_with_baseline.jl $t $l $max_gens $alpha".($baseline ? " 1" : "");
   say $command;
   do {
     $output = `pinpoint -i 100 -- $command 2>&1`;
     say $output;
-    ( $gpu, $pkg, $seconds ) = process_pinpoint_output_intel $output;
+    ( $ram, $pkg, $foo, $gpu, $foo, $seconds ) = process_pinpoint_output_intel $output;
   } while ( $gpu == 0 );
   say "$pre_preffix$preffix, $function, $t,  $l,$pkg, $seconds";
   my ($generations, $best_fitness, $target_fitness, $evaluations ) = process_bna_output( $output );
-  my @results = ($pkg,$seconds,$generations, $best_fitness-$target_fitness, $evaluations);
+  my @results = ($ram,$pkg,$seconds,$generations, $best_fitness-$target_fitness, $evaluations);
   say join(", ", @results);
   say $fh "$pre_preffix$function, $t, $l, $max_gens, $alpha, ", join(", ", @results), ", ", join(", ", @initial_temperature);
 }
